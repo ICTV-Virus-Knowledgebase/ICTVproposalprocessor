dc36f550b8b4aa03f778341502a880a34eb85ce4
Curtis Hendrickson <curtish@uab.edu>
2025-01-30 00:31:39 -0600

    use git commit date for version date: v#.YYYYMMDD.HASH

--- version_git.sh
+++ version_git.sh
@@ -2,5 +2,5 @@
 #
 # get git hash and append to version.txt
 #
-echo "$(cat version.txt).$(date +"%Y%m%d").$(git log -1| head -1 | cut -c 8-14)" > version_git.txt
+echo "$(cat version.txt).$(git log -1 --date=format:'%Y%m%d'| awk '/^Date:/{print $2}').$(git log -1| egrep "^commit " | cut -c 8-14)" > version_git.txt
 echo "VERSION: $(cat version.txt)  ($(cat version_git.txt))"
