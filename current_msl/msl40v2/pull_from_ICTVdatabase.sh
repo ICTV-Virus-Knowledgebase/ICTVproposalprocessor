97d97189117f266299a7aeae3e5915de85d8b068
mimsLR <lmims52@uab.edu>
2026-06-09 21:34:16 +0000

    Create msl41v1_20260320 and pull down the data - pull_from_ICTVdatabase will clone the release tag of the folder directory or by using a MSL_PREFIX input

--- current_msl/msl40v2/pull_from_ICTVdatabase.sh
+++ current_msl/msl40v2/pull_from_ICTVdatabase.sh
@@ -2,13 +2,31 @@
 #
 # clone ICTVdatabase repo, and pull out data files and copy here
 #
+set -euo pipefail
+
 REPO_URL="git@github.com:ICTV-Virus-Knowledgebase/ICTVdatabase.git"
 TEMP=$(mktemp -d) 
 REPO_DIR="$TEMP/ICTVdatabase"
-REPO_DIR="/Users/curtish/Documents/ICTV/ICTVdatabase"
 
-# clone from github
-#git clone $REPO_URL $REPO_DIR
+# Let the caller override it, otherwise derive it from the folder name.
+# For current_msl/msl41v1 this becomes MSL41.v1
+MSL_PREFIX="${MSL_PREFIX:-$(basename "$PWD" | sed -E 's/^msl([0-9]+)v([0-9]+)(_[0-9]{8})?$/MSL\1.v\2/')}"
+
+# Find the newest tag that matches this MSL release.
+TAG="$(
+  git ls-remote --tags --refs "$REPO_URL" "refs/tags/${MSL_PREFIX}_*" \
+    | awk '{print $2}' \
+    | sed 's#refs/tags/##' \
+    | sort -V \
+    | tail -n 1
+)"
+
+if [ -z "$TAG" ]; then
+  echo "ERROR: no tag found for prefix ${MSL_PREFIX}_" >&2
+  exit 1
+fi
+
+git clone --branch "$TAG" --single-branch --depth 1 "$REPO_URL" "$REPO_DIR"
 
 # copy here
 for file in $(ls $REPO_DIR/data/*.utf8.txt); do
